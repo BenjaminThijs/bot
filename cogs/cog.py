320b93db18cbc2177b6042102dd5d77a4d8b592d
flopown <thijsbenjamin@live.be>
2020-10-10 08:33:24 +0200

    Added the monitor_loop, which is already broken thanks to discord

--- cogs/cog.py
+++ cogs/cog.py
@@ -1,4 +1,7 @@
 import json
+import os
+import time
+
 from discord.ext import commands, tasks
 
 
@@ -10,14 +13,53 @@ class Cog(commands.Cog):
         self.bot = bot
 
     def save_settings(self):
+        # This will update the settings file to be the same as self.settings
+        # This is an override so some settings might get lost
+        # TODO: do some backup thing here?
         with open("settings.json", "w") as settings_file:
             json.dump(self.settings, settings_file, indent=4)
 
-    @tasks.loop(seconds=10)
+    # This code will run once every hour (if something needs to be monitored)
+    @tasks.loop(hours=1)
     async def monitor_loop(self):
+        print(f"Starting monitor cycle {self.monitor_loop.current_loop} at {time.ctime(time.time())}")
 
-        members = await guild.fetch_members(limit=150).flatten()
-        # joined_at, id, roles, display_name
+        # TODO: we receive a 403 Missing Acces when we try to use fetch_members in any way
+        # TODO: this is likely due to changes in discord itself, might be fixed once discord.py updates?
+        print("Skipping since the 403 issue still breaks this code")
+        return
+
+        for guild_id in self.settings["monitor"]:
+            # Get the actual guild from the id
+            # TODO: what if the bot got removed from a server it should be monitoring?
+            guild = await self.bot.fetch_guild(guild_id)
+
+            print(f"Starting monitoring cycle for server {guild.name} at {time.ctime(time.time())}")
+
+            # This folder is where the files for this server will be stored
+            # NOTE: we make the foldername both the id and the name, this way it is both readable for a user and still based on id
+            folder = f"monitor/{guild_id}_{guild.name}"
+
+            # Make sure we have a folder in which we can put the info for this server
+            os.makedirs(folder, exist_ok=True)
+
+            # Save the info we just retrieved into a json file
+            # The json filename will be timebased to ensure no duplicates and the proper order
+            with open(f"{folder}/{time.time_ns()}.json", "w") as monitor_file:
+                # Get all the members on the server
+                # We don't need all the info that is provided by the api
+                json.dump([
+                    {
+                        "id": m.id,
+                        "roles": [{"role_id": r.id, "role": r.name} for r in m.roles if not r.name == "@everyone"],
+                        "display_name": m.display_name,
+                        "nickname": m.nick
+                    } async for m in guild.fetch_members()],
+                    monitor_file, indent=4)
+
+        print(f"Finished monitor cycle {self.monitor_loop.current_loop} at {time.ctime(time.time())}")
+
+        # TODO: also monitor the audit logs?
         # https://discordpy.readthedocs.io/en/latest/api.html?highlight=ctx#discord.Guild.audit_logs
         pass
 
