a3ac88589906b33fbb3816f90ecae215219bf3d1
flopown <thijsbenjamin@live.be>
2020-10-10 11:05:53 +0200

    Moved main code to main script

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from client import Client
+import bot
+import cogs
 
+# Just making sure the token is in a private file that will never be pushed
 def retrieve_token():
     with open("token", "r") as token_file:
         return token_file.read()
 
-if __name__ == "__main__":
-    client = Client()
+# Create the bot instance
+bot = bot.Bot(command_prefix="$", case_insensitive=True)
 
-    try:
-        client.run(retrieve_token())
-    except:
-        client.close()
+# Run the botloop, in case we stop the bot by force we have a try except around it.
+try:
+    bot.run(retrieve_token())
+except KeyboardInterrupt:
+    bot.close()
