4989b049961f73f03a681ffaf72fd6556c88f3da
flopown <thijsbenjamin@live.be>
2020-10-13 12:30:48 +0200

    Added some logging

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+import os
+import json
+import datetime
+
+import discord
+from discord import Embed, Color
 from discord.ext import commands
 
 import cogs
@@ -7,76 +13,177 @@ class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        with open("settings.json", "r") as settings_file:
-            self.settings = json.load(settings_file)
+        try:
+            with open("monitor.json", "r") as monitor_file:
+                self.watcher = json.load(monitor_file)
+        except:
+            # json doesnt exist
+            self.watcher = {}
+            self.save_watcher()
 
         for c in cogs.all_cogs:
             self.add_cog(eval(c)(self))
 
-    def save_settings(self):
-        # This will update the settings file to be the same as self.settings
-        # This is an override so some settings might get lost
+    def save_watcher(self):
+        # This will update the monitor file to be the same as self.watcher
+        # This is an override so some monitor might get lost
         # TODO: do some backup thing here?
-        with open("settings.json", "w") as settings_file:
-            json.dump(self.settings, settings_file, indent=4)
+        with open("monitor.json", "w") as monitor_file:
+            json.dump(self.watcher, monitor_file, indent=4)
 
-    def can_check_guild(self, guild_id):
+    def can_check_guild(self, guild):
         # Is this guild part of the monitored list?
-        return guild_id in self.settings["monitor"]
+        return self.watcher.get(str(guild.id), None)
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
-    # MESSAGES
-    async def on_message_delete(self, message):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(message.guild.id):
+    # Watcher logging method
+    async def log(self, guild, embed):
+        # If the author is the bot, ignore it
+        # If this message doesnt come from a guild, ignore it
+        # If the guild is not in the monitored list we shouldn't log anything
+        if embed.author == str(self.user) or guild is None or (channel_id := self.can_check_guild(guild)) is None:
             return
 
+        # We need to retrieve the channel from the id first
+        channel = await self.fetch_channel(channel_id)
+
+        # Send the log
+        await channel.send(
+            embed=embed
+        )
+
+    # MESSAGES
+    # TODO: what to do with files?
+    async def on_message_delete(self, message):
+        await self.log(
+            guild=message.guild,
+            embed=Embed(
+                description=message.content,
+                color=Color.red()
+            ).set_author(
+                name=str(message.author)
+            ).set_footer(
+                text="deleted"
+            )
+        )
+
     async def on_message_edit(self, before, after):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(before.guild.id):
-            return
+        try:
+            await self.log(
+                guild=before.guild,
+                embed=Embed(
+                    color=Color.orange()
+                ).set_author(
+                    name=str(before.author)
+                ).add_field(
+                    name="Before",
+                    value=before.content
+                ).add_field(
+                    name="After",
+                    value=after.content
+                ).set_footer(
+                    text="edited"
+                )
+            )
+        except discord.errors.HTTPException:
+            # Not sure why this keeps getting thrown, something to do with the add_fields
+            # The code still works though
+            r"""
+            Ignoring exception in on_message_edit
+            Traceback (most recent call last):
+            File "C:\Users\flopown\AppData\Local\Programs\Python\Python39\lib\site-packages\discord\client.py", line 333, in _run_event
+                await coro(*args, **kwargs)
+            File "D:\mass\projects\bot\bot.py", line 67, in on_message_edit
+                await self.log(
+            File "D:\mass\projects\bot\bot.py", line 47, in log
+                await channel.send(
+            File "C:\Users\flopown\AppData\Local\Programs\Python\Python39\lib\site-packages\discord\abc.py", line 904, in send
+                data = await state.http.send_message(channel.id, content, tts=tts, embed=embed,
+            File "C:\Users\flopown\AppData\Local\Programs\Python\Python39\lib\site-packages\discord\http.py", line 245, in request
+                raise HTTPException(r, data)
+            discord.errors.HTTPException: 400 Bad Request (error code: 50035): Invalid Form Body
+            In embed.fields.0.value: This field is required
+            In embed.fields.1.value: This field is required
+            """
+            pass
 
     # MEMBERS
     async def on_member_join(self, member):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(member.guild.id):
-            return
+        await self.log(
+            guild=member.guild,
+            embed=Embed(
+                color=Color.green()
+            ).set_author(
+                name=str(member)
+            ).set_footer(
+                text="joined"
+            )
+        )
 
     async def on_member_remove(self, member):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(member.guild.id):
-            return
+        await self.log(
+            guild=member.guild,
+            embed=Embed(
+                color=Color.red()
+            ).set_author(
+                name=str(member)
+            ).set_footer(
+                text="left / kicked"
+            )
+        )
 
     async def on_member_update(self, before, after):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(before.guild.id):
-            return
+        pass
 
     # BANS
     async def on_member_ban(self, guild, user):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(guild.id):
-            return
+        await self.log(
+            guild=guild,
+            embed=Embed(
+                color=Color.red()
+            ).set_author(
+                name=str(user)
+            ).set_footer(
+                text="banned"
+            )
+        )
 
     async def on_member_unban(self, guild, user):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(guild.id):
-            return
+        await self.log(
+            guild=guild,
+            embed=Embed(
+                color=Color.green()
+            ).set_author(
+                name=str(user)
+            ).set_footer(
+                text="unbanned"
+            )
+        )
 
     # ROLES
     async def on_guild_role_create(self, role):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(role.guild.id):
-            return
+        await self.log(
+            guild=role.guild,
+            embed=Embed(
+                description=role.name,
+                color=Color.green()
+            ).set_footer(
+                text="role added"
+            )
+        )
 
     async def on_guild_role_delete(self, role):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(role.guild.id):
-            return
+        await self.log(
+            guild=role.guild,
+            embed=Embed(
+                description=role.name,
+                color=Color.red()
+            ).set_footer(
+                text="role deleted"
+            )
+        )
 
     async def on_guild_role_update(self, before, after):
-        # If the guild is not in the monitored list we shouldn't catch this event
-        if not self.can_check_guild(before.guild.id):
-            return    
+        pass 
--- cogs/admin.py
+++ cogs/admin.py
@@ -12,9 +12,8 @@ class Admin(cog.Cog):
             await ctx.send(
                 "**Monitor / Watch**\n\nMonitor allows me to view who is a member of the server.\n" +
                 "I will periodically check this information, this allows me to tell you when a person has left the server.\n\n" +
-                f"If you want me to monitor this server, use the command `{ctx.prefix}monitor start`\n" +
-                f"If you want me to stop monitoring this server, use the command `{ctx.prefix}monitor stop`\n" +
-                f"If you want me to give you the latest changes to the server, use the command `{ctx.prefix}monitor status`"
+                f"If you want me to monitor this server, use the command `{ctx.prefix}monitor start` in the channel where you want me to log.\n" +
+                f"If you want me to stop monitoring this server, use the command `{ctx.prefix}monitor stop` anywhere in the server."
             )
         elif ctx.guild is None:
             # A subcommand has been entered, but the message was not sent in a guild
@@ -23,16 +22,17 @@ class Admin(cog.Cog):
     @monitor.command(name="start", brief="Start monitoring this server")
     @cog.is_guild_owner()
     async def monitor_start(self, ctx):
-        # We only care about the id of the server
+        # We only care about the id of the server and channel
         guild_id = ctx.guild.id
+        channel_id = ctx.channel.id
 
-        if guild_id in self.bot.settings["monitor"]:
+        if guild_id in self.bot.watcher.keys():
             # The server is already in our list to be monitored
             await ctx.send("I'm already monitoring this server!")
         else:
             # Add the server to the list to be monitored
-            self.bot.settings["monitor"].append(guild_id)
-            self.bot.save_settings()
+            self.bot.watcher[str(guild_id)] = channel_id
+            self.bot.save_watcher()
 
             await ctx.send(f"I'm now monitoring `{ctx.guild.name}`!")
 
@@ -42,32 +42,27 @@ class Admin(cog.Cog):
         # We only care about the id of the server
         guild_id = ctx.guild.id
 
-        if not guild_id in self.bot.settings["monitor"]:
+        if not guild_id in self.bot.watcher.keys():
             # This server is not in our list of monitored servers
             await ctx.send("I'm not monitoring this server")
         else:
             # Remove the server from the list to be monitored
-            self.bot.settings["monitor"].remove(guild_id)
-            self.bot.save_settings()
+            self.bot.watcher.pop(guild_id)
+            self.bot.save_watcher()
 
             await ctx.send(f"I have stopped monitoring `{ctx.guild.name}`")
-
-    @monitor.command(name="status", brief="Show the latest changes on this server")
-    @cog.is_guild_owner()
-    async def monitor_status(self, ctx):
-        await ctx.send("Work in progress :)")
 
     @commands.command(name="stop", aliases=["exit", "off"], brief="Turn off the bot")
     async def stop(self, ctx):
         await ctx.send("Bye bye")
-        await self.bot.close()
+        await self.bot.logout()
 
     @commands.command(name="reload", brief="Reload all cogs")
     async def reload(self, ctx):
         for c in list(self.bot.cogs):
             self.bot.remove_cog(c)
 
-        for c in cogs.all_cogs:
+        for c in self.bot.cogs.all_cogs:
             self.bot.add_cog(eval(c)(self.bot))
 
         await ctx.send("Done reloading")
